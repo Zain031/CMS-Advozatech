f101ffbfb0f7117d2d57c9d246e8e366994c7fe3
al1skey <adamnurramadan@gmail.com>
2025-01-01 20:13:56 +0700

    fix: add missing semicolon in closeModal button click handler

--- src/pages/category/index.jsx
+++ src/pages/category/index.jsx
@@ -216,7 +216,7 @@ const Category = () => {
                                         >
                                             {/* if there is a button in form, it will close the modal */}
                                             <button onClick={(e)=>{
-                                                e.preventDefault()
+                                                e.preventDefault();
                                                 closeModal()}} className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
                                                 ✕
                                             </button>
